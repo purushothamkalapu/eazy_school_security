1b41def1843906485422135d4597f59bd80de5c2
Sri Purushothama <ksripurushotham@yahoo.com>
2023-11-02 17:44:09 +0530

    Video 23 & 24

--- springsecsection3/src/main/java/com/purushotham/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/purushotham/config/ProjectSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.purushotham.config;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,5 +28,37 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        //Approach 1
+
+        /*UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);*/
+
+        //Approach 2
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .roles("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .roles("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
