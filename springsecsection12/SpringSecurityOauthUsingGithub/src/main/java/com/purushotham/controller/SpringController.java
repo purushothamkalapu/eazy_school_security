98a809275f153d09b04e8c93caf771b30705e0c4
Purushotham <ksripurushotham@yahoo.com>
2023-11-20 07:07:32 +0530

    Section 12 OAuth2 done

---
+++ springsecsection12/SpringSecurityOauthUsingGithub/src/main/java/com/purushotham/controller/SpringController.java
@@ -0,0 +1,15 @@
+package com.purushotham.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+    @GetMapping("/")
+    public String main(OAuth2AuthenticationToken token){
+        System.out.println(token.getPrincipal());
+        return "security.html";
+    }
+}
